2fec4472186e9c4cace096556edd6320535f43b5
charliesands <cwsands3@gmail.com>
2018-09-20 14:27:35 -0600

    latest build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   baseUrl: '/battle-cards/',
   outputDir: 'docs',
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined
 }
