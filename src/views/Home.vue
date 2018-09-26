4ddb2a4a0af3640d1244a7468b17ba573cd51693
charliesands <cwsands3@gmail.com>
2018-09-25 19:47:29 -0600

    playing with style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="home">
+  <div class="home container-fluid">
     <h1>BattleCards</h1>
+    <div class="row">
+      <div class="col-6 offset-3 align-items-center">
+        <form v-on:submit.prevent="startGame" class="">
+          Enter Name: <input type="text" v-model="game.gameConfig.playerName" placeholder="Your Name"> <br># of
+          Opponents: <input type="number" v-model="game.gameConfig.opponents" placeholder="# of Opponents"><br> Deck #:
+          <input type="number" v-model="game.gameConfig.set" placeholder="Set #">
+          <button @click="startGame">Start</button>
+          <button type="button" @click="gameList">Games</button>
 
-    <form v-on:submit.prevent="startGame">
-      Enter Name: <input type="text" v-model="game.gameConfig.playerName" placeholder="Your Name"> <br># of Opponents: <input
-        type="number" v-model="game.gameConfig.opponents" placeholder="# of Opponents"><br> Deck #: <input type="number" v-model="game.gameConfig.set"
-        placeholder="Set #">
-      <button @click="startGame">Start</button>
-      <button type="button" @click="gameList">Games</button>
-
-    </form>
+        </form>
+      </div>
+    </div>
     <div>
       <p v-for="game in gamesList">
         <!-- Game ID: {{game._id}} -->
